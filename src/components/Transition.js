7960365882b1b142489248cdfc555163278f47e4
Barri <info@bdesignstudio.co.uk>
2020-07-07 18:43:29 +0100

    init project

---
+++ src/components/Transition.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class TransitionComp extends Component{
+    state ={
+        show: false
+    }
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show? true: false
+        })
+    }
+    render(){
+        return(
+            <div>
+                {this.state.show?<div style={{
+                    background: 'red',
+                    height:'100px'
+                }}>
+
+                </div> : null}
+                <div className="showDiv" 
+                onClick={this.showDiv}>
+                    show or hide
+                </div>
+            </div>
+
+        )
+    }
+}
+
+
+export default TransitionComp;
